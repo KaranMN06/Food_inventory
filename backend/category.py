7d575654c56a235694467e1920bf242e8c36812a
KaranMN06 <karanmn06@gmail.com>
2024-03-19 01:52:02 +0530

    patch

---
+++ backend/category.py
@@ -0,0 +1,67 @@
+from fastapi import FastAPI
+from fastapi import APIRouter
+from schemas.category_schema import Category
+from fastapi.encoders import jsonable_encoder
+import os
+from fastapi.responses import JSONResponse
+import json
+
+
+category_app=APIRouter()
+
+category_data={}
+
+category_path='C:/Users/kmysore/Desktop/project2/Food_inventory/backend/responses/category.json'
+
+'''POST API for category management'''
+
+@category_app.post("/category-create/",tags=['Category Management'])
+async def create_category(category:Category):
+    category_data = jsonable_encoder(category)
+    
+    if os.path.exists(category_path) and os.path.getsize(category_path) >0:
+        with open(category_path,'r') as f:
+            res = json.load(f)
+        print(res)
+        res_key= list(res.keys())
+        
+        if ((str(category_data['category_name'])).lower()) in res_key:
+            detail={
+                    "message": f"{category.category_name} is already present",
+                    "reason": "duplicate attribute",
+                    "referenceError": None,
+                    "code": "notFound"
+                    }
+            return JSONResponse(content = detail, status_code=409)
+        
+        else:
+            res[(str(category.category_name)).lower()] = (category_data['status']).lower()
+            with open(category_path,'w') as f:
+                json.dump(res,f,indent = 4)
+            return JSONResponse(content = res,status_code = 201)
+    else:
+        with open(category_path,'w') as f:
+            res_data={}
+            res_data[(str(category.category_name)).lower()] = (category_data['status']).lower()
+            json.dump(res_data,f,indent=4)
+            
+        return JSONResponse(content=res_data,status_code = 201)    
+     
+    
+'''PATCH API for category management'''
+@category_app.patch("/category-modify/",tags=['Category Management'])
+async def modify_category(category:Category):
+    # try:
+        res_data={}
+        res_data[(str(category.category_name)).lower()] = (category_data['status']).lower()
+        with open(category_path,'r') as file:
+            json_data=json.load(file)
+        
+        for item in json_data.keys():
+            if item==category.category_name:
+                json_data[item]=res_data[category_name]
+                with open(category_path,'w') as resp_file:
+                    json.dump(json_data,resp_file,indent=4)
+            else:
+                return JSONResponse(content='category doesnt match',status_code = 422)        
+    
